d8b6a031801dcd83ac5a7d8a0ac03f408d7e3412
markjohn519 <markjohnmonton@gmail.com>
2022-11-22 12:15:47 +0800

    modified iterable with working 0

--- iterable.js
+++ iterable.js
@@ -1,7 +1,7 @@
 function createIterable(num) {
   let start = 0
   let step = 1
-  let value = step
+  let value = start
 
   return {
     [Symbol.iterator]() {
@@ -9,15 +9,12 @@ function createIterable(num) {
         next() {
           let result
           // eslint-disable-next-line no-unreachable-loop
-          for (let i = 1; i < num;) {
-            if (start === 0) {
-              console.log(start)
-            }
-            result = { value, done: false }
-            value = start + step
+          while (num > 0) {
+            value += start
             start = step
             step = value
             num -= 1
+            result = { value, done: false }
             return result
           }
           return { value, done: true }
